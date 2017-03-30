536fe81715a4181309f4382cb053afa919941df9
agarcia_21 <agarcia_21@att.net>
2017-03-30 11:37:53 -0700

    respect the KeyboardDisplayRequiresUserAction setting (config.xml)

--- src/ios/CDVWKWebViewEngine.m
+++ src/ios/CDVWKWebViewEngine.m
@@ -299,6 +299,19 @@ static void * KVOContext = &KVOContext;
     } else {
         [wkWebView.scrollView setDecelerationRate:UIScrollViewDecelerationRateFast];
     }
+
+    BOOL keyboardRequiresUserAction = ([settings cordovaBoolSettingForKey:@"KeyboardDisplayRequiresUserAction" defaultValue:NO]);
+
+    if (!keyboardRequiresUserAction) {
+        SEL sel = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:userObject:");
+        Class WKContentView = NSClassFromString(@"WKContentView");
+        Method method = class_getInstanceMethod(WKContentView, sel);
+        IMP originalImp = method_getImplementation(method);
+        IMP imp = imp_implementationWithBlock(^void(id me, void* arg0, BOOL arg1, BOOL arg2, id arg3) {
+            ((void (*)(id, SEL, void*, BOOL, BOOL, id))originalImp)(me, sel, arg0, TRUE, arg2, arg3);
+        });
+        method_setImplementation(method, imp);
+    }
 }
 
 - (void)updateWithInfo:(NSDictionary*)info
